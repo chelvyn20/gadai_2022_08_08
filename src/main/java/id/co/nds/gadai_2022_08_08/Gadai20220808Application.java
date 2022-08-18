4f9b61bf2c0ea974e40da05760f6d9a6df23f188
chelvyn20 <chelvyn20@gmail.com>
2022-08-18 09:38:48 +0700

    first commit

---
+++ src/main/java/id/co/nds/gadai_2022_08_08/Gadai20220808Application.java
@@ -0,0 +1,13 @@
+package id.co.nds.gadai_2022_08_08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gadai20220808Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gadai20220808Application.class, args);
+	}
+
+}
